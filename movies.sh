b5aa6223e7a29712400453093c578908342871c9
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2024-04-06 16:57:31 -0400

    Delete movies index

--- movies.sh
+++ movies.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Delete movies index
+curl -XDELETE localhost:9200/movies
+
+echo -e "\n---\n"
+
 # Create movies index
 curl -H "Content-Type: application/json" -XPUT localhost:9200/movies -d '
 {
